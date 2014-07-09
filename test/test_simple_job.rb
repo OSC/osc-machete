7df9668d2c35d165505085abca3ab80c73dfe09a
Eric Franz <efranz@osc.edu>
2014-07-09 16:06:47 -0400

    add more asserts for SimpleJob unit tests
    assert that an instance of a class that includes SimpleJob
    responds to the expected methods

--- test/test_simple_job.rb
+++ test/test_simple_job.rb
@@ -15,5 +15,20 @@ class TestSimpleJob < Minitest::Test
       
       include OSC::Machete::SimpleJob
     end
+    
+    obj = cls.new
+    
+    # verify responds to status methods
+    assert_respond_to obj, :submitted?
+    assert_respond_to obj, :completed?
+    assert_respond_to obj, :status_human_readable
+    assert_respond_to obj, :update_status!
+    
+    # verify responds to submit methods
+    assert_respond_to obj, :staging_template_name
+    assert_respond_to obj, :crimson_files_dir_name
+    assert_respond_to obj, :staging
+    assert_respond_to obj, :submit
   end
 end
+
