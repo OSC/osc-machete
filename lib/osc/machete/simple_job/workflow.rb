ca6c72e12cde42735cd7a1fc76f00cffc040885a
Jeremy Nicklas <jnicklas@osc.edu>
2015-01-30 12:04:37 -0500

    fixed variable assignment of staged_dir

--- lib/osc/machete/simple_job/workflow.rb
+++ lib/osc/machete/simple_job/workflow.rb
@@ -2,13 +2,13 @@ module OSC
   module Machete
     module SimpleJob
       module Workflow
-        
+
         def has_machete_workflow_of(jobs_active_record_relation_symbol)
           # yes, this is magic mimicked from http://guides.rubyonrails.org/plugins.html
           #  and http://yehudakatz.com/2009/11/12/better-ruby-idioms/
           cattr_accessor :jobs_active_record_relation_symbol
           self.jobs_active_record_relation_symbol = jobs_active_record_relation_symbol
-          
+
           # separate modules to group common methods for readability purposes
           # both builder methods and status methods need the jobs relation so
           # we include that first
@@ -16,20 +16,20 @@ module OSC
           self.send :include, OSC::Machete::SimpleJob::Workflow::BuilderMethods
           self.send :include, OSC::Machete::SimpleJob::Workflow::StatusMethods
         end
-        
+
         module JobsRelation
           def jobs_active_record_relation
             self.send self.class.jobs_active_record_relation_symbol
           end
         end
-        
+
         # depends on jobs_active_record_relation being defined
         module BuilderMethods
           #FIXME: this should be a constant provided by the app: data_root
           def data_root
             OSC::Machete::Crimson.new(Rails.application.class.parent_name).files_path
           end
-        
+
           def staging_template_name
             self.class.name.underscore
           end
@@ -39,28 +39,28 @@ module OSC
           def staging_target_dir_name
             staging_template_name.pluralize
           end
-          
+
           def staging_target_dir
             data_root.join(staging_target_dir_name)
           end
-          
+
           def stage
             staging_template_dir = Rails.root.join("jobs", staging_template_name)
-        
+
             staged_dir = OSC::Machete::JobDir.new(staging_target_dir).new_jobdir
             FileUtils.mkdir_p staged_dir
             FileUtils.cp_r staging_template_dir.to_s + "/.", staged_dir
-        
+
             staged_dir
           end
-      
+
           def render_mustache_files(staged_dir, template_view)
             Location.new(staged_dir).render(template_view)
           end
 
           def after_stage(staged_dir)
           end
-          
+
           # returns an array of unsubmitted OSC::Machete::Job objects with their dependencies (if any) configured
           def build_jobs(staged_dir, jobs = [])
             raise NotImplementedError, "Objects including OSC::Machete::SimpleJob::Workflow must implement build_jobs"
@@ -71,9 +71,9 @@ module OSC
           end
 
           def save_jobs(jobs)
-            self.staged_dir_path = staged_dir.to_s if self.respond_to?(:staged_dir=)
+            self.staged_dir = staged_dir.to_s if self.respond_to?(:staged_dir=)
             self.save if self.id.nil? || self.respond_to?(:staged_dir=)
-            
+
             jobs.each do |job|
               self.jobs_active_record_relation.create(job: job)
             end
@@ -89,7 +89,7 @@ module OSC
             save_jobs(jobs)
           end
         end
-        
+
         # depends on jobs_active_record_relation being defined
         module StatusMethods
           def submitted?
@@ -129,7 +129,7 @@ module OSC
             end
           end
         end
-        
+
         # extend Active Record with the has_workflow_of method
         ActiveRecord::Base.extend OSC::Machete::SimpleJob::Workflow if defined? ActiveRecord::Base
       end
