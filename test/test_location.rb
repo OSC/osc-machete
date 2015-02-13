0ef69f34bb863d2ca99df4790f724658582135b0
Eric Franz <ctangent@gmail.com>
2015-02-13 14:01:49 -0500

    refactor test to use mktmpdir for all tmp dirs
    this test was copying to a new directory that wasn't created using
    mktmpdir so this fixes that; also move some custom setup/teardown
    to the setup/teardown methods

--- test/test_location.rb
+++ test/test_location.rb
@@ -4,82 +4,83 @@ require 'osc/machete'
 class TestLocation < Minitest::Test
 
   def setup
-    @target = Dir.mktmpdir("location")
-    @subject = OSC::Machete::Location.new @target
+    # tmp directory
+    # and we are making a location object that wraps that directory
+    # we add mustache templates and test rendering
+    # we add another tmp directory/location and we test copy to
+    @dir1 = Dir.mktmpdir("location")
+    @dir2 = Dir.mktmpdir("location")
+    @location1 = OSC::Machete::Location.new(@dir1)
+    @location2 = OSC::Machete::Location.new(@dir2)
   end
 
   def teardown
-    FileUtils.remove_entry @target
+    FileUtils.remove_entry @dir1
+    FileUtils.remove_entry @dir2
   end
 
   # Location.render(params, options = {})
 
   def test_render_default_replace_template
     setup_render
-    @subject.render("")
-    assert_equal true,  File.exists?("#{@target}/file.txt")
-    assert_equal false, File.exists?("#{@target}/file.txt.mustache")
+    @location1.render("")
+    assert_equal true,  File.exists?("#{@dir1}/file.txt")
+    assert_equal false, File.exists?("#{@dir1}/file.txt.mustache")
   end
 
   def test_render_user_replace_template
     setup_render
-    @subject.render("", {replace: true})
-    assert_equal true,  File.exists?("#{@target}/file.txt")
-    assert_equal false, File.exists?("#{@target}/file.txt.mustache")
+    @location1.render("", {replace: true})
+    assert_equal true,  File.exists?("#{@dir1}/file.txt")
+    assert_equal false, File.exists?("#{@dir1}/file.txt.mustache")
   end
 
   def test_render_user_doesnt_replace_template
     setup_render
-    @subject.render("", {replace: false})
-    assert_equal true,  File.exists?("#{@target}/file.txt")
-    assert_equal true,  File.exists?("#{@target}/file.txt.mustache")
+    @location1.render("", {replace: false})
+    assert_equal true,  File.exists?("#{@dir1}/file.txt")
+    assert_equal true,  File.exists?("#{@dir1}/file.txt.mustache")
   end
 
   def test_copy_to_shouldnt_copy_developer_files
     setup_copy_to
-    begin
-      @subject.copy_to(@new_target)
-      assert_equal true,  Dir.exists?("#{@new_target}")
-      assert_equal false, Dir.exists?("#{@new_target}/.git")
-      assert_equal false, Dir.exists?("#{@new_target}/.svn")
-      assert_equal false, Dir.exists?("#{@new_target}/test.dir.1")
-      assert_equal true,  Dir.exists?("#{@new_target}/test.dir.2")
-      assert_equal true,  File.exists?("#{@new_target}/test.1")
-      assert_equal false, File.exists?("#{@new_target}/test.2")
-      assert_equal true,  File.exists?("#{@new_target}/test.3")
-      assert_equal false, File.exists?("#{@new_target}/test.4")
-      assert_equal false, File.exists?("#{@new_target}/test.dir.2/test.1")
-      assert_equal false, File.exists?("#{@new_target}/test.dir.2/test.2")
-      assert_equal true,  File.exists?("#{@new_target}/test.dir.2/test.4")
-      assert_equal false, File.exists?("#{@new_target}/.gitignore")
-    ensure
-      teardown_copy_to
-    end
+    
+    @location1.copy_to(@dir2)
+    assert_equal true,  Dir.exists?("#{@dir2}")
+    assert_equal false, Dir.exists?("#{@dir2}/.git")
+    assert_equal false, Dir.exists?("#{@dir2}/.svn")
+    assert_equal false, Dir.exists?("#{@dir2}/test.dir.1")
+    assert_equal true,  Dir.exists?("#{@dir2}/test.dir.2")
+    assert_equal true,  File.exists?("#{@dir2}/test.1")
+    assert_equal false, File.exists?("#{@dir2}/test.2")
+    assert_equal true,  File.exists?("#{@dir2}/test.3")
+    assert_equal false, File.exists?("#{@dir2}/test.4")
+    assert_equal false, File.exists?("#{@dir2}/test.dir.2/test.1")
+    assert_equal false, File.exists?("#{@dir2}/test.dir.2/test.2")
+    assert_equal true,  File.exists?("#{@dir2}/test.dir.2/test.4")
+    assert_equal false, File.exists?("#{@dir2}/.gitignore")
   end
   
   
   private
 
     def setup_render
-      FileUtils.touch("#{@target}/file.txt.mustache")
+      FileUtils.touch("#{@dir1}/file.txt.mustache")
     end
 
     def setup_copy_to
-      # Destination of copied files
-      @new_target = "#{@target}_1"
-
       # Example directory/file structure of developer
-      Dir.mkdir("#{@target}/.git")
-      Dir.mkdir("#{@target}/.svn")
-      Dir.mkdir("#{@target}/test.dir.1")
-      Dir.mkdir("#{@target}/test.dir.2")
-      FileUtils.touch("#{@target}/test.1")
-      FileUtils.touch("#{@target}/test.2")
-      FileUtils.touch("#{@target}/test.3")
-      FileUtils.touch("#{@target}/test.4")
-      FileUtils.touch("#{@target}/test.dir.2/test.1")
-      FileUtils.touch("#{@target}/test.dir.2/test.2")
-      FileUtils.touch("#{@target}/test.dir.2/test.4")
+      Dir.mkdir("#{@dir1}/.git")
+      Dir.mkdir("#{@dir1}/.svn")
+      Dir.mkdir("#{@dir1}/test.dir.1")
+      Dir.mkdir("#{@dir1}/test.dir.2")
+      FileUtils.touch("#{@dir1}/test.1")
+      FileUtils.touch("#{@dir1}/test.2")
+      FileUtils.touch("#{@dir1}/test.3")
+      FileUtils.touch("#{@dir1}/test.4")
+      FileUtils.touch("#{@dir1}/test.dir.2/test.1")
+      FileUtils.touch("#{@dir1}/test.dir.2/test.2")
+      FileUtils.touch("#{@dir1}/test.dir.2/test.4")
 
       # Make example .gitignore
       gitignore = <<-END.gsub(/^ {20}/, '')
@@ -91,10 +92,6 @@ class TestLocation < Minitest::Test
                     test.dir.2/test.1
                   END
 
-      File.open("#{@target}/.gitignore", 'w') {|f| f.write(gitignore) }
-    end
-
-    def teardown_copy_to
-      FileUtils.remove_entry @new_target
+      File.open("#{@dir1}/.gitignore", 'w') {|f| f.write(gitignore) }
     end
 end
