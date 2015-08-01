a2f96db0ebc254d6bb56d5c3792ab806f4838480
Eric Franz <efranz@osc.edu>
2015-08-01 15:20:38 -0400

    add class description to Job

--- lib/osc/machete/job.rb
+++ lib/osc/machete/job.rb
@@ -1,26 +1,71 @@
 require 'pathname'
-# do we have to do OSC::Appkit:: for every class we create in a gem?
-# or can we just have require be inside a module?
+
+
+# Core object for working with batch jobs, including:
+#
+# * submitting jobs
+# * checking job status
+# * setting dependencies between jobs via a directed acyclic graph
+#
+# Create a new Job from a script:
+#
+#     job = Job.new(script: '/nfs/17/efranz/jobs/1/script.sh')
+#     job.submitted? #=> false
+#     job.path #=> '/nfs/17/efranz/jobs/1'
+#     job.script_name #=> 'script.sh'
+#     job.status #=> nil
+#     job.pbsid #=> nil
+#
+#     job.submit
+#     job.submitted? #=> true
+#     job.status #=> "Q"
+#     job.pbsid #=> "3422735.oak-batch"
+#
+#     # if you know the pbs id you can instantiate a
+#     # Job object to ask for the status of it
+#     job2 = Job.new(pbsid: "3422735.oak-batch")
+#     job2.status #=> "Q"
+#
+#     # because the object was created with only the pbsid passed in,
+#     # path and script_name and dependency information is not available
+#     job2.path #=> nil
+#     job2.script_name #=> nil
+#
+#     # but an unknown pbsid results in status nil
+#     job3 = Job.new(pbsid: "12345.oak-batch")
+#     job3.status #=> nil
+#
+# Create two Job instances and form a dependency between them:
+#
+#     job1 = Job.new(script: '/nfs/17/efranz/jobs/1/script.sh')
+#     job2 = Job.new(script: '/nfs/17/efranz/jobs/1/post.sh')
+#
+#     job2.afterany(job1) # job2 runs after job1 completes with any exit status
+#
+#     job1.submit
+#     job2.submit
+#
+#     job1.status #=> "Q"
+#     job2.status #=> "H"
+#
+
 class OSC::Machete::Job
   attr_reader :pbsid, :script_path
-  
+
   # Create new job closure
-  # 
+  #
   # Takes params in options hash as single argument:
-  # 
+  #
   #     Job.new(script: '/path/to/job/dir/go.sh')
-  # 
+  #
   # or
-  # 
+  #
   #     opts = Hash.new(script: '/path/to/job/dir/go.sh')
   #     Job.new(opts)
-  # 
+  #
   # Job class makes assumption that a job's PBS_O_WORKDIR will be
   # in the directory containing the shell script that is run. 
-  # TODO: Is there ever a situation where this is not the case?
-  # If so, we can revert to separate path and script arguments or
-  # add special case arguments to handle this situation.
-  # 
+  #
   # @param [Hash] args the arguments to create the job
   # @option args [String] :script  full path to script (required)
   # @option args [String, nil] :pbsid   pbsid of a job already submitted (optional)
@@ -31,33 +76,33 @@ class OSC::Machete::Job
   def initialize(args)
     @script_path = Pathname.new(args[:script]).cleanpath unless args[:script].nil?
     # @script_path = @script_path.expand_path would change this to absolute path
-    
+
     @pbsid =  args[:pbsid]
     @torque = args[:torque_helper] || OSC::Machete::TorqueHelper.new()
-    
+
     @dependencies = {} # {:afterany => [Job, Job], :afterok => [Job]}
   end
-  
+
   # name of the script
   def script_name
     Pathname.new(@script_path).basename.to_s if @script_path
   end
-  
+
   # path to job directory 
   def path
     Pathname.new(@script_path).dirname if @script_path
   end
-  
+
   #TODO: needs more robust solution here, for error checking, etc.
   #
   # submit any dependent jobs that haven't been submitted
   # then submit this job, specifying dependencies as required
   def submit
     return if submitted?
-    
+
     # submit any dependent jobs that have not yet been submitted
     submit_dependencies
-    
+
     # cd into directory, submit job from there
     # so that PBS_O_WORKDIR is set to location
     # where job is run
@@ -65,64 +110,64 @@ class OSC::Machete::Job
       @pbsid = @torque.qsub script_name, depends_on: dependency_ids
     end
   end
-  
+
   def submitted?
     ! @pbsid.nil?
   end
-  
+
   def status
     @torque.qstat @pbsid unless @pbsid.nil?
   end
-  
+
   # TODO: moving caching status/workflow into here
   # or keep in other objects outside of Job?
   # def status_as_string(status)
   #   {:Q => "Queued", :H => "Hold", :R => "Running"}.fetch(status, "Completed")
   # end
-  
+
   # creates Job#afterany(jobs) and Job#afterok(jobs) etc.
   # each method can accept a Job instance or an Array of Job instances
   [:afterany, :afterok, :after, :afternotok].each do |type|
     define_method(type) do |jobs|
       @dependencies[type] = [] unless @dependencies.has_key?(type)
       @dependencies[type].concat(Array(jobs))
-      
+
       self
     end
   end
-  
+
   def delete(rmdir: false)
     # FIXME: using a keyword argument here... should we also do so in the initializer
     # instead of the hash parameter
-    
+
     # FIXME: rethink this interface... should qdel be idempotent? 
     # After first call, no errors thrown after?
     if pbsid && @torque.qdel(pbsid)
       # FIXME: removing a directory is always a dangerous action.
       # I wonder if we can add more tests to make sure we don't delete
       # something if the script name is munged
-      
+
       # recursively delete the directory after killing the job
       Pathname.new(path).rmtree if path && rmdir && File.exists?(path)
     end
   end
-  
+
   private
-  
+
   def submit_dependencies
     #  assumes each dependency is a Job object
     @dependencies.values.flatten.each { |j| j.submit }
   end
-  
+
   # build a dictionary of ids for each dependency type
   def dependency_ids
     ids = {}
-    
+
     @dependencies.each do |type, jobs|
       ids[type] = jobs.map(&:pbsid).compact
     end
-    
+
     ids.keep_if { |k,v| ! v.empty? }
   end
-  
+
 end
