7358a48a7490f1a0fd8d6f2dd58d0498bc0b9862
Eric Franz <efranz@osc.edu>
2015-08-17 10:54:30 -0400

    document location

--- lib/osc/machete/location.rb
+++ lib/osc/machete/location.rb
@@ -1,41 +1,54 @@
 require 'pathname'
 require 'mustache'
 
-# FIXME: Location has two methods of interest:
-# copy_to and render; otherwise, could we just use
-# Pathname or URI to refer to a location?
-# Is this extra class necessary?
+# A util class with methods used with staging a simulation template directory.
+# Use it by wrapping a file path (either string or Pathname object). 
+# For example, if I have a template directory at "/nfs/05/efranz/template"
+# I can recursivly copy the template directory:
+#
+#     target = "/nfs/05/efranz/simulations/1"
+#     simulation = Location.new("/nfs/05/efranz/template").copy_to(target)
+#
+# Then I can recursively render all the mustache templates in the copied directory,
+# renaming each file from XXXX.mustache to XXXX:
+#
+#     simulation.render(iterations: 20, geometry: "/nfs/05/efranz/geos/fan.stl")
+#
 class OSC::Machete::Location
   # URIs, Paths, rendering, and copying
   # this should be refactored into separate objects
-  
+
   # @param path  either string, Pathname, or Machete::Location object
   def initialize(path)
     @path = Pathname.new(path.to_s).cleanpath
     @template_ext = ".mustache"
   end
-  
+
   # @return [String] The location path as String.
   def to_s
     @path.to_s
   end
-  
+
   # Copies the data in a Location to a destination path using rsync. 
-  # 
-  # @param [String] dest The target location as path string. 
-  # 
-  # @return [String] The target location path.
+  #
+  # @param [String, Pathname] dest The target location path. 
+  # @return [Location] The target location path wrapped by Location instance.
   def copy_to(dest)
     # @path has / auto-dropped, so we add it to make sure we copy everything
     # in the old directory to the new
     destloc = self.class.new(dest)
     `rsync -r --exclude='.svn' --exclude='.git' --exclude='.gitignore' --filter=':- .gitignore' #{@path.to_s}/ #{destloc.to_s}`
-    
+
     # return target location so we can chain method
     destloc
   end
-  
-  # return list of template files in directory (recursively searched)
+
+  # **This should be a private method**
+  #
+  # Get a list of template files in this Location, where a template file is a
+  # file with the extension .mustache
+  #
+  # @return [Array<String>] list of template files in directory (recursively searched)
   def template_files
     if @path.directory?
       Dir.glob(File.join(@path, "**/*#{@template_ext}"))
@@ -43,30 +56,33 @@ class OSC::Machete::Location
       @path.to_s.end_with? @template_ext ? [@path.to_s] : []
     end
   end
-  
+
+  # Render each mustache template and rename the file, removing the extension
+  # that indicates it is a template file i.e. `.mustache`.
+  #
+  # @return [self] returns self for optional chaining
   def render(params, options = {})
     # custom_delimiters = options['delimeters'] || nil
     replace_template_files = options[:replace].nil? ? true : options[:replace]
 
     renderer = Mustache.new
-    
+
     template_files.each do |template|
       rendered_file = template.chomp(@template_ext)
-      
+
       rendered_string = nil
       File.open(template, 'r') do |f|
         rendered_string = renderer.render(f.read, params)
       end
       # boo...
       # rendered_string = renderer.render_file(template, params)
-      
+
       File.open(rendered_file, 'w') { |f| f.write(rendered_string) }
-      
+
       FileUtils.rm template if replace_template_files
     end
-    
+
     # return self so this can be at the end of a chained method
     self
   end
-  
 end
