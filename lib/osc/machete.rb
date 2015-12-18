bdb7f027bea386cdf650d37b4e8fc97cb2626fe1
Eric Franz <efranz@osc.edu>
2015-12-18 14:31:17 -0500

    remove simple_job

--- lib/osc/machete.rb
+++ lib/osc/machete.rb
@@ -8,10 +8,6 @@ require "osc/machete/torque_helper"
 require "osc/machete/staging"
 require "osc/machete/user"
 require "osc/machete/process"
-require "osc/machete/simple_job"
-require "osc/machete/simple_job/submittable"
-require "osc/machete/simple_job/statusable"
-require "osc/machete/simple_job/workflow"
 
 module OSC
   # The main osc-machete module.
