bff80a82a798f389e4784910bc8235d8a7a557df
Brian McMichael <bmcmichael@osc.edu>
2016-01-06 12:03:32 -0500

    Test class that submits a job, stats it, and deletes it. Tests with mocks are not working properly.

--- test/test_torque_helper.rb
+++ test/test_torque_helper.rb
@@ -19,8 +19,11 @@ class TestTorqueHelper < Minitest::Test
   
   def setup
    # @xml = '<Data><Job><Job_Id>16376372.opt-batch.osc.edu</Job_Id><Job_Name>stage.pbs.sh</Job_Name><Job_Owner>efranz@websvcs06.osc.edu</Job_Owner><job_state>Q</job_state><queue>serial</queue><server>opt-batch.osc.edu:15001</server><Checkpoint>u</Checkpoint><ctime>1386618379</ctime><Error_Path>websvcs06.osc.edu:/nfs/17/efranz/crimson_files/EweldPredictor/11/stage.pbs.error</Error_Path><Hold_Types>n</Hold_Types><Join_Path>n</Join_Path><Keep_Files>n</Keep_Files><Mail_Points>a</Mail_Points><mtime>1386618379</mtime><Output_Path>websvcs06.osc.edu:/nfs/17/efranz/crimson_files/EweldPredictor/11/stage.pbs.output</Output_Path><Priority>0</Priority><qtime>1386618379</qtime><Rerunable>True</Rerunable><Resource_List><arch>x86_64</arch><nodect>1</nodect><nodes>1:ppn=8</nodes><walltime>10:00:00</walltime></Resource_List><Shell_Path_List>/bin/sh</Shell_Path_List><Variable_List>PBS_O_HOME=/nfs/17/efranz,PBS_O_LANG=C,PBS_O_LOGNAME=root,PBS_O_PATH=/sbin:/usr/sbin:/bin:/usr/bin,PBS_O_MAIL=/var/spool/mail/epi,PBS_O_SHELL=/bin/bash,PBS_SERVER=opt-batch:15001,PBS_O_WORKDIR=/nfs/17/efranz/crimson_files/EweldPredictor/11,PBS_O_QUEUE=batch,PBS_O_HOST=websvcs06.osc.edu</Variable_List><etime>1386618379</etime><submit_args>-S /bin/sh stage.pbs.sh</submit_args><Walltime><Remaining>3600</Remaining></Walltime><fault_tolerant>False</fault_tolerant></Job></Data>'
-    #@job_state = OSC::Machete::Status.queued
-    #@shell = OSC::Machete::TorqueHelper.new
+    @job_state_queued = OSC::Machete::Status.queued
+    @job_state_completed = OSC::Machete::Status.completed
+    @job_state_running = OSC::Machete::Status.running
+
+    @shell = OSC::Machete::TorqueHelper.new
 
     # FIXME: Tests are expecting all methods to use :qstat_xml
     #@shell.stubs(:qstat_xml).returns(@xml)
@@ -33,44 +36,63 @@ class TestTorqueHelper < Minitest::Test
     # directory where to create jobs
     #@target = Dir.mktmpdir
     #@script = 'GLO_job'
+
+    @script_oakley = 'test/fixtures/oakley.sh'
+    @script_ruby = "#PBS -N unit_ruby\n#PBS -l walltime=00:03:00\n#PBS -l nodes=1:ppn=1\n#PBS -q @ruby-batch.osc.edu\n#PBS -j oe\n cal"
+    @script_default = "#PBS -N unit_default\n#PBS -l walltime=00:04:00\n#PBS -l nodes=1:ppn=1\n#PBS -j oe\n cal"
   end
-  
+
+  # Test qstat against system for completed job.
+  def test_qstat_state_job_completed
+    @shell = OSC::Machete::TorqueHelper.new
+
+    pbsid = '16376372.oak-batch.osc.edu'
+    assert_equal @job_state_completed, @shell.qstat(pbsid)
+    #assert_not @job_state_queued, @shell.qstat(pbsid)
+    assert_equal @job_state_completed, @shell.qstat(nil)
+
+  end
+
+  # Test job state parser when returning queued
   def test_qstat_state_job_avail
-    #pbsid = '16376372.oak-batch.osc.edu'
-    #pbs_conn = mock(PBS::Conn.batch('oakley'))
-    #pbs_job = mock(PBS::Job.new(conn: pbs_conn, id: pbsid))
-    #pbs_job.any_instance.stubs(:status[:attribs][:job_state]).returns(OSC::Machete::Status.queued)
+    shell = OSC::Machete::TorqueHelper.new
 
+    pbsid = '16376372.oak-batch.osc.edu'
+    shell.expects(:job_state).returns("Q")
+    assert_equal @job_state_queued, @shell.qstat(pbsid)
+    shell.expects(:job_state).returns("R")
+    assert_equal @job_state_running, @shell.qstat(pbsid)
+    shell.expects(:job_state).returns("C")
+    assert_equal @job_state_completed, @shell.qstat(pbsid)
 
-    # FIXME: Not using xml
-    # @shell.stubs(:qstat_xml).returns(@xml)
-    # FIXME: The new machete returns Queued, not "Q"
-    # assert_equal @job_state.to_s, @shell.qstat('16376372.opt-batch.osc.edu')
-    #assert_equal @job_state, @shell.qstat('16376372.opt-batch.osc.edu')
-    return true
   end
   
   def test_qstat_state_no_job
+    pbsid = '16376372.oak-batch.osc.edu'
+    @shell = OSC::Machete::TorqueHelper.new
+    @shell.expects(:job_state).returns(nil)
+    assert_equal @job_state_completed, @shell.qstat(nil)
+    assert_equal @job_state_completed, @shell.qstat("")
     #@shell.stubs(:qstat_xml).returns("")
     #assert_equal OSC::Machete::Status.completed, @shell.qstat('16376372.opt-batch.osc.edu')
-    return true
+    #return true
   end
 
+  # This tests an actual live workflow by
+  #   submitting a job to oakley,
+  #   checking it's status, and
+  #   deleting it.
   def test_qsub_oakley
-    # test actual shell command used i.e. in backticks
-    #
-    # EFHACK: because the shell command now requires a prefix to successfully
-    # submit from Glenn webservice nodes to Oakley (and presumably Ruby), we
-    # verify that the shell command ends with what is expected. This makes all
-    # the existing tests work. A better approach should be considered during
-    # refactor/rewrite.
-    #
-    # thank you mocha: http://gofreerange.com/mocha/docs/Mocha/Expectation.html#with-instance_method
-    #
-    # FIXME: Commented this out because we're using PBS
-    # @shell.expects(:`).with() {|v| v.end_with? "qsub -q @oak-batch.osc.edu test/fixtures/oakley.sh"}.returns("16376372.oak-batch.osc.edu\n")
-    #@shell.qsub("test/fixtures/oakley.sh")
-    return true
+
+    live_job = @shell.qsub(@script_oakley)
+    assert_match /\d+.oak-batch.osc.edu/, live_job
+
+    live_status = @shell.qstat(live_job)
+    assert_equal @job_state_queued, live_status
+
+    delete_status = @shell.qdel(live_job)
+    assert_equal true, delete_status
+
   end
   
   def test_qsub_glenn
