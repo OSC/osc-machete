c6a84c41ef0f7a8ce4e136a1714510c42b3b6ce4
Brian McMichael <bmcmichael@osc.edu>
2016-01-11 15:03:30 -0500

    more doc

--- lib/osc/machete/status.rb
+++ lib/osc/machete/status.rb
@@ -66,37 +66,66 @@ class OSC::Machete::Status
 
   # Returns the status as the char value.
   #
+  # @example Running
+  #     OSC::Machete::Status.running.to_s #=> "R"
+  #
   # @return [String] The status char
   def to_s
     @char.to_s
   end
 
-
+  # Return a readable string of the status
+  #
+  # @example Running
+  #     OSC::Machete::Status.running.inspect #=> "Running"
+  #
+  # @return [String] The status value as a formatted string
   def inspect
     # FIXME: ActiveSupport  replace with .humanize and simpler datastructure
      VALUES_HASH[@char].split("_").map(&:capitalize).join(" ")
   end
 
+  # Return the a StatusValue object based on the highest precedence of the two objects.
+  #
+  # @example One job is running and a dependent job is queued.
+  #   OSC::Machete::Status.running + OSC::Machete::Status.queued #=> Running
+  #
+  # Return [OSC::Machete::Status] The max status by precedence
   def +(other)
     [self, other].max
   end
 
+  # The comparison operator for sorting values.
+  #
+  # @return [Integer] Comparison value based on precedence
   def <=>(other)
     precedence <=> other.precedence
   end
 
+  # Boolean evaluation of Status object equality.
+  #
+  # @return [Boolean] True if the values are the same
   def eql?(other)
     other.to_s == to_s
   end
 
+  # Boolean evaluation of Status object equality.
+  #
+  # @return [Boolean] True if the values are the same
   def ==(other)
     self.eql?(other)
   end
 
+  # Return a hash based on the char value of the object.
+  #
+  # @return [Fixnum] A hash value of the status char
   def hash
     @char.hash
   end
 
+  # Return the ordinal position of the status in the precidence list
+  #
+  # @return [Integer] The order of precedence for the object
   def precedence
     # Hashes enumerate their values in the order that the corresponding keys were inserted
     PRECEDENCE.index(@char)
