73e0bc4ad0248e89e8905fe7f4c8b3e06f8e3d21
Eric Franz <efranz@osc.edu>
2016-02-01 14:49:21 -0500

    remove deprecated Staging class
    it is only used by Submittable, which we removed

--- lib/osc/machete.rb
+++ lib/osc/machete.rb
@@ -6,7 +6,6 @@ require "osc/machete/job"
 require "osc/machete/job_dir"
 require "osc/machete/location"
 require "osc/machete/torque_helper"
-require "osc/machete/staging"
 require "osc/machete/user"
 require "osc/machete/process"
 
